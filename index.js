2b9190bb0018fa7697a754643a7210a34d03b102
blackbackpacks <1017926959@qq.com>
2019-05-23 22:55:14 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+    leftSwipe()
+    rightSwipe()
+}
+
+var leftSwipe = function(){
+    var leftBox = document.querySelector('.left-navbar')
+    var childBox = leftBox.querySelector('ul')  
+    
+    childBox.addEventListener('touchstart',function(e){})
+    childBox.addEventListener('touchstart',function(e){})
+    childBox.addEventListener('touchstart',function(e){})
+}
